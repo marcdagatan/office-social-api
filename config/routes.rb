65523168257826c12847766792935d811cdcee59
Marc Dagatan <marc.dagatan@gmail.com>
2022-01-17 16:10:53 +0800

    Add registrations and sessions controllers and routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
 end
