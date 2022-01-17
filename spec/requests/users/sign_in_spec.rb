fa9e6f019538d68978292b5b4a1d9bce6bb5bdd6
Marc Dagatan <marc.dagatan@gmail.com>
2022-01-17 20:02:19 +0800

    Add failure spec for sign in

--- spec/requests/users/sign_in_spec.rb
+++ spec/requests/users/sign_in_spec.rb
@@ -7,11 +7,20 @@ describe "POST /users/sign_in", type: :request do
   let(:params) { { user: { email: email, password: password } } }
   let!(:user) { create(:user, email: email, password: password) }
 
-  fit "allows a user to sign in" do
+  it "allows a user to sign in" do
     post url, params: params, as: :json
 
     expect(response).to have_http_status(:success)
     expect(response.header).to have_key("Authorization")
     expect(json[:user]).to include(email: email)
+  end
+
+  it "returns unauthorized with an error message if credentials is wrong" do
+    params[:user][:password] = "this is the wrong password"
+
+    post url, params: params, as: :json
+
+    expect(response).to have_http_status(:unauthorized)
+    expect(json[:error]).to eq "Invalid Email or password."
   end
 end
